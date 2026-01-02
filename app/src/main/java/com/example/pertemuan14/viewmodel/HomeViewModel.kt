076988bc0f0efcca0012a7294331510f8d3815e2
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 18:29:01 +0700

    menambahkan StatusUiSiswa untuk state loading data siswa

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/HomeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan14.modeldata.Siswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import java.io.IOException
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa> = listOf()) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
