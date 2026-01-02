f060a6a3548746d3f9f6e5d21aa8b47bdb467574
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 20:38:13 +0700

    hasil

--- app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/pertemuan14/view/controllNavigasi/PetaNavigasi.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.pertemuan14.view.EntrySiswaScreen
 import com.example.pertemuan14.view.HomeScreen
 import com.example.pertemuan14.view.route.DestinasiDetail
 import com.example.pertemuan14.view.route.DestinasiEntry
