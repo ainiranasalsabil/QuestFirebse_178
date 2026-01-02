addbd9aada8273781b168ca5696a88340910512f
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 18:32:55 +0700

    menambahkan validasi input data siswa

--- app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -17,4 +17,27 @@ class EntryViewModel(
         private set
 
     /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+        }
+    }
+}
