4b480efcce4d146ac218649b47319a49db279c55
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 18:10:38 +0700

    menambahkan interface RepositorySiswa

---
+++ app/src/main/java/com/example/pertemuan14/repositori/RepositorySiswa.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan14.repositori
+
+import com.example.pertemuan14.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
