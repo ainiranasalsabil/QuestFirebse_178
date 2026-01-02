ec50e0f0f7840789f0814d55ea26603bebbb4928
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 18:31:55 +0700

    menambahkan EntryViewModel untuk input data siswa

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan14.modeldata.DetailSiswa
+import com.example.pertemuan14.modeldata.UIStateSiswa
+import com.example.pertemuan14.modeldata.toDataSiswa
+import com.example.pertemuan14.repositori.RepositorySiswa
+
+class EntryViewModel(
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
