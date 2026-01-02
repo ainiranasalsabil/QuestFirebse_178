c6e1f0e3c1adcfceed76825d4d8a585f4a77d0aa
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-02 18:22:04 +0700

    menambahkan extension CreationExtras untuk Application

---
+++ app/src/main/java/com/example/pertemuan14/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan14.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan14.repositori.AplikasiDataSiswa
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+
