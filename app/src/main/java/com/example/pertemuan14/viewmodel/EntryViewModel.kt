802d806a17e3971f89c7243c061afece8d109a36
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-09 16:07:49 +0700

    Menyimpan data siswa ke repository jika input valid

--- app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -34,3 +34,12 @@ class EntryViewModel(
         )
     }
 
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+        }
+    }
+}
