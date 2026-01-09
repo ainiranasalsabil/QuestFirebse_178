aebab534a2b742edffd1417ccc3b3938bdd8875c
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-09 16:06:36 +0700

    Mengembalikan true jika semua field wajib sudah terisi

--- app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EntryViewModel.kt
@@ -19,4 +19,10 @@ class EntryViewModel(
     /* Fungsi untuk memvalidasi input */
     private fun validasiInput(
         uiState: DetailSiswa = uiStateSiswa.detailSiswa
-    ):
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() &&
+                    alamat.isNotBlank() &&
+                    telpon.isNotBlank()
+        }
+    }
