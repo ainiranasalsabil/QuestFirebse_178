cc8b5c1522b471671073173e9a5e03b41412a9a7
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-01-09 16:09:45 +0700

    Mengambil ID siswa dari parameter navigasi

--- app/src/main/java/com/example/pertemuan14/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/viewmodel/EditViewModel.kt
@@ -21,3 +21,8 @@ class EditViewModel(
 
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
